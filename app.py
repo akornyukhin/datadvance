2de579e444d92eeabab3953488e65d2017623a4f
akornyukhin <kastatool@gmail.com>
2018-05-24 12:05:05 +0300

    Code fixed based on feedback

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
 import sys
-import pandas as pd 
+import pandas as pd
+import numpy as np
+
+from tqdm import tqdm
 
 def incidents(input_path, output_path, dT=0.3):
     df = pd.read_csv(input_path, index_col='id')
-    df_sorted = df.reset_index().sort_values('time', ascending=False)
+
+    df['feature3'] = (df.feature1 * 100).astype('str') + df.feature2.astype('str')
+    df = df.loc[:,['time','feature3']]
+    df_sorted = df.reset_index().sort_values(['feature3','time'], ascending=False)
+
     d = df_sorted.as_matrix()
 
-    result = {}
     len_ = df_sorted.shape[0]
+    result = np.zeros(shape=(len_))
 
-    for i in range(len_):
+    for i in tqdm(range(len_)):
         count = 0
         for j in range(i+1, len_):
-            if d[i,1] == d[j,1] and d[i,2] == d[j,2] and abs(d[i,3] - d[j,3]) <= dT:
-                count += 1
-            else:
-                break
-        result[d[i,0]] = count
+                if abs(d[i,1] - d[j,1]) <= dT:
+                    if d[i,2] == d[j,2]:
+                        count += 1
+                    else:
+                        break
+        result[int(d[i,0])] = count
 
-    df_result = pd.DataFrame.from_dict(result, orient='index')
+    df_result = pd.DataFrame(result, columns=['count'], dtype='int32')
     df_result = df_result.reset_index().astype('int').sort_values('index').rename(columns={'index': 'id', 0: 'count'})
-    df_result.to_csv(output_path, index=False)
+    df_result.to_csv(output_path, index=False, index_label='id', sep=';')
     
     return print('You can see the file in the directory you have chosen')
 
